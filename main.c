853851f1ba80c176270e73d4f4b620d2a226ffe8
sowon0627-byte <sowon0627@sookmyung.ac.kr>
2025-10-30 16:04:59 +0900

    09_03_2

--- main.c
+++ main.c
@@ -6,17 +6,20 @@
 int main(void)
 {
     int i;
-    int grade[SIZE];
-    int score[SIZE];
-     
-    for(i=0;i<SIZE;i++)
-       grade[i] = rand() % 100;
-       
-    for(i=0;i<SIZE;i++)
-       score[i] = grade[i];
+    int a[SIZE] = {1,2,3,4,5};
+    int b[SIZE] = {1,8,3,4,5};
+    int flag_same = 1;
     
     for(i=0;i<SIZE;i++)
-       printf("grade[%d] = %d, score[%d] = %d\n", i, grade[i], i, score[i]); 
+    {
+        if(a[i] != b[i])
+        {
+           printf("%i-th element is diff.\n",i);
+           flag_same = 0;
+        }
+    }
+    
+    printf("Are a and b the same? %i\n",flag_same);
  
   system("PAUSE");	
   return 0;
