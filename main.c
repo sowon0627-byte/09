25f5eb69156016d99dc4a5b497ba765d0cfcc1cc
sowon0627-byte <sowon0627@sookmyung.ac.kr>
2025-10-30 15:46:36 +0900

    09_02

--- main.c
+++ main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 5
+
 int main(void)
 {
-    int grade[5];
+    int grade[SIZE];
     int i;
+    int sum = 0;
     
-    grade[0] = 10;
-    grade[1] = 20;
-    grade[2] = 30;
-    grade[3] = 40;
-    grade[4] = 50;
+    // 점수 입력 // 
+    for(i=0;i<SIZE;i++)
+    {
+       printf("enter student grade: ");
+       scanf("%d", &grade[i]);
+       sum = sum+grade[i];
+    }
     
-    for(i=0;i<5;i++)
-    printf("grade[%d] = %d\n", i, grade[i]);
+    // 점수 출력 // 
+    printf("average is %d\n", sum / SIZE);
   
   system("PAUSE");	
   return 0;
